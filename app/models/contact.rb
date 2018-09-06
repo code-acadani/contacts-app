cad8852584c9922e40451ad4cc5c98ede9e4f852
dani <dani.zaghian@gmail.com>
2018-09-06 10:07:13 -0700

    adds validators and happy sad paths

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
+
+	validates :first_name, :last_name, presence: true
+	validates :email, uniqueness: true
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	# validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}
+
 	def friendly_updated_at
 		updated_at.strftime("%B %e, %Y")
 	end
